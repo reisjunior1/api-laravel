be1be71c99982da6e13c938b23c125e2e332209d
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2021-09-15 16:27:05 -0300

    primeiro teste

---
+++ tests/Feature/ContaTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Conta;
+
+class ContaTest extends TestCase
+{
+    /** @test */
+    public function check_se_colunas_em_contas_esta_correto()
+    {
+        $conta = new Conta;
+
+        $expect = [
+            'numero',
+        ];
+        $array_comparacao = array_diff($expect, $conta->getFillable());
+        //dd($array_comparacao);
+        $this->assertEquals(0, count($array_comparacao));
+    }
+}
